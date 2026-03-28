4f486574aa06577b1502486872f03a3674d21f03
david-ptrk <davidptrk7@gmail.com>
2026-03-28 13:32:43 +0500

    resolver updated for try, class, continue, break, assign

--- src/resolver.py
+++ src/resolver.py
@@ -18,9 +18,10 @@ from src.statements import StmtVisitor
 class Resolver(ExprVisitor, StmtVisitor):
     def __init__(self, interpreter):
         self.interpreter = interpreter
-        self.scopes = []
+        self.scopes = [{}]
         self.current_class = None
         self.current_function = None
+        self.loop_depth = 0
     
     # Entry Point
     def resolve(self, statements):
@@ -62,7 +63,7 @@ class Resolver(ExprVisitor, StmtVisitor):
                 return
         # Not Found -> Global
     
-    def resolve_function(self, func):
+    def resolve_function(self, func, func_type):
         self.begin_scope()
         
         for param in func.params:
@@ -93,8 +94,10 @@ class Resolver(ExprVisitor, StmtVisitor):
             self.resolve_stmt(stmt.else_branch)
     
     def visit_while_stmt(self, stmt):
+        self.loop_depth += 1
         self.resolve_expr(stmt.condition)
         self.resolve_stmt(stmt.body)
+        self.loop_depth -= 1
     
     def visit_return_stmt(self, stmt):
         if self.current_function is None:
@@ -132,15 +135,23 @@ class Resolver(ExprVisitor, StmtVisitor):
         self.resolve_local(expr, expr.name)
     
     def visit_assign_expr(self, expr):
+        if self.scopes:
+            self.ensure_declared(expr.name)
+        
         self.resolve_expr(expr.value)
         
         if self.scopes:
-            scope = self.scopes[-1]
-            # If variable doesn't exist in current scope, declare it
-            if expr.name.lexeme not in scope:
-                scope[expr.name.lexeme] = True
+            self.mark_initialized(expr.name)
         
         self.resolve_local(expr, expr.name)
+    
+    def ensure_declared(self, name):
+        scope = self.scopes[-1]
+        if name.lexeme not in scope:
+            scope[name.lexeme] = False
+    
+    def mark_initialized(self, name):
+        self.scopes[-1][name.lexeme] = True
     
     def visit_call_expr(self, expr):
         self.resolve_expr(expr.callee)
@@ -154,7 +165,7 @@ class Resolver(ExprVisitor, StmtVisitor):
         enclosing_function = self.current_function
         self.current_function = "METHOD" if self.current_class else "FUNCTION"
         
-        self.resolve_function(stmt)
+        self.resolve_function(stmt, self.current_function)
         self.current_function = enclosing_function
     
     def visit_class_stmt(self, stmt):
@@ -166,6 +177,7 @@ class Resolver(ExprVisitor, StmtVisitor):
         self.current_class = "CLASS"
         
         self.begin_scope()
+        self.scopes[-1]["this"] = True
         for statement in stmt.body:
             self.resolve_stmt(statement)
         
@@ -173,16 +185,41 @@ class Resolver(ExprVisitor, StmtVisitor):
         self.current_class = enclosing_class
     
     def visit_memberaccess_expr(self, expr):
-        pass
+        self.resolve_expr(expr.object)
     
     def visit_for_stmt(self, stmt):
-        pass
+        self.resolve_expr(stmt.iterable)
+        self.begin_scope()
+        
+        # self.scopes[-1][stmt.var.lexeme] = True
+        self.declare(stmt.var)
+        self.define(stmt.var)
+        
+        self.loop_depth += 1
+        self.resolve_stmt(stmt.body)
+        self.loop_depth -= 1
+        
+        self.end_scope()
     
     def visit_try_stmt(self, stmt):
-        pass
+        self.resolve_stmt(stmt.try_block)
+        
+        for exception_var, block in stmt.except_blocks:
+            self.begin_scope()
+            
+            if exception_var is not None:
+                self.scopes[-1][exception_var.lexeme] = True
+            
+            self.resolve_stmt(block)
+            self.end_scope()
+        
+        if stmt.finally_block is not None:
+            self.resolve_stmt(stmt.finally_block)
     
     def visit_continue_stmt(self, stmt):
-        pass
+        if self.loop_depth == 0:
+            raise Exception("Cannot use 'continue' outside loop")
     
     def visit_break_stmt(self, stmt):
-        pass
+        if self.loop_depth == 0:
+            raise Exception("Cannot use 'break' outside loop")
